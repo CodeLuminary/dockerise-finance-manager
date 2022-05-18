81a42f88f41279326479d9b360945136b03817bf
CodeLuminary <victorijoni@yahoo.com>
2022-05-18 21:05:52 +0100

    Header added for frontend

---
+++ react_app/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import navbar_style from "../css/navbar.module.css";
+import {Link} from "react-router-dom";
+
+const Navbar = ()=>{
+    return (
+        <div className={navbar_style.nav}>
+            <ul>
+                <li><Link to="/">Income/Expenses</Link></li>
+                <li><Link to="/">Assets/Liabilities</Link></li>
+                <li><Link to="/">Financial Statement</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ react_app/src/views/home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar";
+import {Routes, Route} from "react-router-dom";
+
+const Home = ()=>{
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                
+            </Routes>
+        </>
+    )
+}
+
+export default Home;
