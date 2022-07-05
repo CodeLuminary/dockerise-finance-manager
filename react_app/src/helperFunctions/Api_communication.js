2d48ad8fadfd22bf5c784003f56667b643864cd8
CodeLuminary <victorijoni@yahoo.com>
2022-07-05 22:22:17 +0100

    Api communication added

---
+++ react_app/src/helperFunctions/Api_communication.js
@@ -0,0 +1,25 @@
+import Api from "./Api";
+
+export const AddIncomeExpense = (dataObject)=>{
+  Api.PostApi('/add-income-expense',dataObject)
+  .then(result=>{
+    if(result.isSuccessful) alert("Income/Expense added successfully")
+    else alert("Action failed. Income/Expense could not be added")
+  })
+}
+
+export const changeSetting = (dataObject)=>{
+    Api.PostApi('/change-settings',dataObject)
+  .then(result=>{
+      if(result.isSuccessful) alert("Settings change successfully")
+      else alert("Action failed. Settings could not be changed")
+  })
+}
+
+export const AddAssetsLiability = (dataObject)=>{
+    Api.PostApi('/add-asset-liability',dataObject)
+    .then(result=>{
+        if(result.isSuccessful) alert("Asset/Liability added successfully")
+      else alert("Action failed. Asset/Liability could not be added")
+    })
+}
