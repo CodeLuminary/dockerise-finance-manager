68497985baedc23d53b6a0f35e229aeb11e6c021
CodeLuminary <victorijoni@yahoo.com>
2022-07-09 12:51:22 +0100

    income_expense updated

--- react_app/src/components/income_expense.jsx
+++ react_app/src/components/income_expense.jsx
@@ -1,29 +1,53 @@
+import { useState } from "react";
 import inc_exp_style from "../css/inc_exp.module.css";
+import {AddIncomeExpense} from "../helperFunctions/Api_communication.js";
 
 const IncomeExpense = ()=>{
+    const [expenditureType, setExpenditureType] = useState("expense");
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [recurrency, setRecurrency] = useState(0);
+    const [datetime, setDateTime] = useState('');
+    const [isRecurrent, setIsRecurrent] = useState(false);
+
+    const HandleSubmit = (e)=>{
+        e.preventDefault();
+        AddIncomeExpense({
+            type: expenditureType,
+            title,
+            description,
+            amount,
+            is_recurrency: isRecurrent,
+            recurrency,
+            transaction_date: datetime
+        })
+    }
+
     return (
-        <div>
+        <form onSubmit={HandleSubmit}>
             <h2>INCOME/EXPENSE</h2>
             <span className={inc_exp_style.label}>Expenditure Type</span>
-            <select className="input">
-                <option>Expense</option>
-                <option>Income</option>
+            <select className="input" onChange={e=>setExpenditureType(e.target.value)}>
+                <option value="expense">Expense</option>
+                <option value="income">Income</option>
             </select><br/>
             <span className={inc_exp_style.label}>Expenditure title/name</span>
-            <input type="text" className="input" placeholder="Enter expenditure name"/><br/>
+            <input onChange={e=>setTitle(e.target.value)} type="text" className="input" placeholder="Enter expenditure name"/><br/>
             <span className={inc_exp_style.label}>Expenditure description</span>
-            <textarea className="textarea"></textarea><br/>
+            <textarea onChange={e=>setDescription(e.target.value)} className="textarea"></textarea><br/>
             <span className={inc_exp_style.label}>Amount</span>
-            <input type="text" className="input" placeholder="Enter amount"/><br/>
+            <input onChange={e=>setAmount(Number(e.target.value))} type="text" className="input" placeholder="Enter amount"/><br/>
             <span className={inc_exp_style.label}>Date/Time</span>
-            <input type="datetime-local" className="input" />
-            <span>Is Expenditure Recurrent</span><input type="checkbox"/><br/>
+            <input on type="datetime-local" onChange={e=>setDateTime(e.target.value)} className="input" />
+            <span>Is Expenditure Recurrent</span>
+            <input type="checkbox" onChange={e=>setIsRecurrent(e.target.checked)}/><br/>
             <div>
                 <span className={inc_exp_style.label}>How Often(in month)</span>
-                <input type="number" className="input"/>
+                <input type="number" onChange={e=>setRecurrency(Number(e.target.value))} className="input"/>
             </div>
-            <button className="btn">Save</button>
-        </div>
+            <input type="submit" className="btn" value="Save"/>
+        </form>
     )
 }
 
