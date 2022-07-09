dee727f7647c4b82aa65e6ce37389442648d4b6b
CodeLuminary <victorijoni@yahoo.com>
2022-07-09 12:42:12 +0100

    Asset_liability updated

--- react_app/src/components/asset_liability.jsx
+++ react_app/src/components/asset_liability.jsx
@@ -4,22 +4,24 @@ import {AddAssetsLiability} from "../helperFunctions/Api_communication.js";
 
 const AssetLiability = ()=>{
     const [typeRate, setTypeRate] = useState('Appreciation');
-    const [incomeType, setIncomeType] = useState("asset");
+    const [propType, setPropType] = useState("asset");
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [amount, setAmount] = useState(0);
     const [duration, setDuration] = useState(0);
     const [rate, setRate] = useState(0.0);
+    const [datetime, setDateTime] = useState('');
 
     const HandleSubmit = (e)=>{
         e.preventDefault();
         AddAssetsLiability({
-            type: incomeType,
+            type: propType,
             title,
             description,
             amount,
             change_duration: duration,
-            change_rate: rate
+            change_rate: rate,
+            transaction_date: datetime
         })
     }
 
@@ -27,25 +29,28 @@ const AssetLiability = ()=>{
         <form onsubmit={HandleSubmit}>
             <h2>INCOME/EXPENSE</h2>
             <span className={inc_exp_style.label}>Type</span>
-            <select className="input">
+            <select className="input" onChange={e=> {
+                    setPropType(e.target.value);
+                    setTypeRate(e.target.value=="asset"? 'Appreciation': 'Depreciation')
+                }}>
                 <option value="asset">Asset</option>
-                <option value="asset">Liability</option>
+                <option value="liability">Liability</option>
             </select><br/>
             <span className={inc_exp_style.label}>Title/Name</span>
-            <input type="text" oninput={e=>setTitle(e.target.value)} className="input" placeholder="Enter expenditure name"/><br/>
+            <input type="text" onChange={e=>setTitle(e.target.value)} className="input" placeholder="Enter expenditure name"/><br/>
             <span className={inc_exp_style.label}>Description</span>
-            <textarea className="textarea" oninput={e=>setDescription(e.target.value)}></textarea><br/>
+            <textarea className="textarea" onChange={e=>setDescription(e.target.value)}></textarea><br/>
             <span className={inc_exp_style.label}>Amount</span>
-            <input type="text" oninput={e=>setAmount(Number(e.target.value))} className="input" placeholder="Enter amount"/><br/>
+            <input type="text" onChange={e=>setAmount(Number(e.target.value))} className="input" placeholder="Enter amount"/><br/>
             <span className={inc_exp_style.label}>Date/Time</span>
-            <input type="datetime-local" className="input" />
+            <input  type="datetime-local" onChange={e=>setDateTime(e.target.value)} className="input" />
             <span className={inc_exp_style.label}>{typeRate} Duration(in month)</span>
-            <input type="number" oninput={e=>setDuration(Number(e.target.value))} value="1" className="input"/><br/>
+            <input type="number" onChange={e=>setDuration(Number(e.target.value))} value="1" className="input"/><br/>
             <span className={inc_exp_style.label}>{typeRate} Rate</span>
-            <input type="text" oninput={e=>setTitle(Number(e.target.value))} placeholder={`Enter rate of ${typeRate} e.g 5 for 5 USD/ month`} className="input"/><br/>
+            <input type="text" onChange={e=>setRate(Number(e.target.value))} placeholder={`Enter rate of ${typeRate} e.g 5 for 5 USD/ month`} className="input"/><br/>
             
             <input className="btn" value="Save"/>
-        </div>
+        </form>
     )
 }
 
