ecf553581498d24bfbcbb33ea8370d3167dbda1b
CodeLuminary <victorijoni@yahoo.com>
2022-07-20 21:39:59 +0100

    Updated

--- app/server.js
+++ app/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-var path = require('path');
+const datetime = require('node-datetime');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const {updateSettings, addAssetLiability, addIncomeExpense} = require('./models/sqlite')
+const {updateSettings, addAssetLiability, addIncomeExpense, getFinancialStatement} = require('./models/sqlite')
 
 app.post('/add-asset-liability',(req,res)=>{
+    //const dt = datetime.create();
+    //req.body.transaction_date = dt.format('Y-m-d H:M:S');
     addAssetLiability(req.body)
     .then(result=>res.send(result));
 })
@@ -21,7 +23,8 @@ app.post('/change-settings',(req,res)=>{
     .then(result=>res.send(result));
 })
 app.post('/get-financial-statement',(req,res)=>{
-    
+    getFinancialStatement(req.body)
+    .then(result=>res.send(result));
 })
 
 app.all('/*',(req,res)=>{
