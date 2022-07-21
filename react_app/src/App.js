4184148ca5da5a2f2e3aa00d73cb61c00b635671
CodeLuminary <victorijoni@yahoo.com>
2022-07-21 13:45:35 +0100

    Routing error corrected

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/*" element={<Home />}/>
         </Routes>
       </Router>
     </div>
