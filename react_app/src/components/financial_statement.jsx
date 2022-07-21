1c21c04ecf2962574c53a13b47ab64ab57cbcdf6
CodeLuminary <victorijoni@yahoo.com>
2022-07-21 13:09:53 +0100

    importation of usestate added

--- react_app/src/components/financial_statement.jsx
+++ react_app/src/components/financial_statement.jsx
@@ -1,14 +1,33 @@
 import inc_exp_style from "../css/inc_exp.module.css";
+import {getFinancialStatement} from "../helperFunctions/Api_communication.js";
+import {useState} from "react";
 
 const FinancialStatement = ()=>{
+    const [financialStatement, setFinancialStatement] = useState({})
+
+    const HandleSumbit=(event)=>{
+        event.preventDefault();
+        getFinancialStatement()
+    }
+
     return (
-        <div>
-            <span className={inc_exp_style.label}>From</span>
-            <input type="date" className="input" /><br/>
-            <span className={inc_exp_style.label}>To</span>
-            <input type="date" className="input" /><br/>
-            <button className="btn">Get Financial Statement</button>
-        </div>
+        <>
+            <form onSubmit={HandleSumbit}>
+                <span className={inc_exp_style.label}>From</span>
+                <input type="date" name="from" className="input" /><br/>
+                <span className={inc_exp_style.label}>To</span>
+                <input type="date" name="to" className="input" /><br/>
+                <input type="submit" className="btn" value="Get Financial Statement"/>
+            </form>
+            <>
+                {financialStatement.income ?
+                (
+                    <>
+                    </>
+                ):``          
+                }
+            </>
+        </>
     )
 }
 
