c43047009fbc52491ea50f3b92d9fa0a2fa7f8ef
CodeLuminary <victorijoni@yahoo.com>
2022-07-21 13:10:17 +0100

    updated

--- react_app/src/components/navbar.jsx
+++ react_app/src/components/navbar.jsx
@@ -6,8 +6,9 @@ const Navbar = ()=>{
         <div className={navbar_style.nav}>
             <ul>
                 <li className={navbar_style.highlight}><Link to="/">Income/Expenses</Link></li>
-                <li><Link to="/">Assets/Liabilities</Link></li>
-                <li><Link to="/">Financial Statement</Link></li>
+                <li><Link to="/asset-liability">Assets/Liabilities</Link></li>
+                <li><Link to="/financial-statement">Financial Statement</Link></li>
+                <li><Link to="/settings">Settings</Link></li>
             </ul>
         </div>
     )
