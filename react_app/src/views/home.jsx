4e57e87bcdf892ee00a8b31d7ee71cb00f2137e9
CodeLuminary <victorijoni@yahoo.com>
2022-07-21 13:09:01 +0100

    route updated

--- react_app/src/views/home.jsx
+++ react_app/src/views/home.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import IncomeExpense from "../components/income_expense";
 import AssetLiability from "../components/asset_liability";
 import Settings from "../components/settings";
+import FinancialStatement from "../components/financial_statement"
 
 const Home = ()=>{
     return (
@@ -13,6 +14,7 @@ const Home = ()=>{
                     <Route index element={<IncomeExpense />}/>
                     <Route path="/asset-liability" element={<AssetLiability/>}/>
                     <Route path="/settings" element={<Settings />}/>
+                    <Route path="/financial-statement" element={<FinancialStatement/>}/>
                 </Routes>
             </div>
         </>
