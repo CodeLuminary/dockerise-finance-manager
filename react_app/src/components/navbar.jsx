b9150c0f4f64e7c6b5ca0bac7668b4f4668f1bf5
CodeLuminary <victorijoni@yahoo.com>
2022-07-24 20:55:33 +0100

    Financial statement added

--- react_app/src/components/navbar.jsx
+++ react_app/src/components/navbar.jsx
@@ -1,14 +1,16 @@
 import navbar_style from "../css/navbar.module.css";
 import {Link} from "react-router-dom";
+import {useState} from "react"
 
 const Navbar = ()=>{
+    const [selected, setSelected] = useState(1);
     return (
         <div className={navbar_style.nav}>
             <ul>
-                <li className={navbar_style.highlight}><Link to="/">Income/Expenses</Link></li>
-                <li><Link to="/asset-liability">Assets/Liabilities</Link></li>
-                <li><Link to="/financial-statement">Financial Statement</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+                <li onClick={()=>setSelected(1)} className={selected==1? navbar_style.highlight: ``}><Link to="/">Income/Expenses</Link></li>
+                <li onClick={()=>setSelected(2)} className={selected==2? navbar_style.highlight: ``}><Link to="asset-liability">Assets/Liabilities</Link></li>
+                <li onClick={()=>setSelected(3)} className={selected==3? navbar_style.highlight: ``}><Link to="/financial-statement">Financial Statement</Link></li>
+                <li onClick={()=>setSelected(4)} className={selected==4? navbar_style.highlight: ``}><Link to="/settings">Settings</Link></li>
             </ul>
         </div>
     )
