d619a607e61eee8b7eadf9aaa92c4b42359635ee
CodeLuminary <victorijoni@yahoo.com>
2022-07-25 04:16:05 +0100

    Updated

--- app/models/sqlite.js
+++ app/models/sqlite.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { resolve } = require('path');
 const location = "./models/fm.db";
 
-let db, dbAll, dbRun;
+let db = new sqlite3.Database(location);let dbAll, dbRun;
 
 //init()
 //seedDb();
@@ -17,20 +17,21 @@ function init() {
 }
 
 async function seedDb(){
-    db = new sqlite3.Database(location);
+    //db = new sqlite3.Database(location);
 
-    let sql = `CREATE TABLE IF NOT EXISTS income_expense (id INT PRIMARY KEY AUTOINCREMENT, type SMALLINT(1), title VARCHAR(100), description TEXT, amount double, transaction_date datetime, is_recurrent SMALLINT(1), recurrency tinyint, PRIMARY KEY(id))`;
-
-    await db.run(sql);
-
-    sql = `CREATE TABLE IF NOT EXISTS asset_liability (id INT PRIMARY KEY AUTOINCREMENT, type SMALLINT(1), title VARCHAR(100), description TEXT, amount double, transaction_date datetime, change_rate double, change_duration int, PRIMARY KEY(id))`;
+    let sql = `CREATE TABLE IF NOT EXISTS income_expense (id INTEGER PRIMARY KEY AUTOINCREMENT, type SMALLINT(1), title VARCHAR(100), description TEXT, amount double, transaction_date int, is_recurrent SMALLINT(1), recurrency tinyint)`;
 
     await db.run(sql);
 
-    sql = `CREATE TABLE IF NOT EXISTS settings (id INT PRIMARY KEY AUTOINCREMENT, currency VARCHAR(10), name VARCHAR(100), logo VARCHAR(100), location VARCHAR(200), PRIMARY KEY(id))`;
+    sql = `CREATE TABLE IF NOT EXISTS asset_liability (id INTEGER PRIMARY KEY AUTOINCREMENT, type SMALLINT(1), title VARCHAR(100), description TEXT, amount double, transaction_date int, change_rate double, change_duration int)`;
+
     await db.run(sql);
 
-    await db.run(`INSERT INTO settings (currency, location) VALUES('USD', 'United State of America')`)
+    sql = `CREATE TABLE IF NOT EXISTS settings (id INTEGER PRIMARY KEY AUTOINCREMENT, currency VARCHAR(10), name VARCHAR(100), logo VARCHAR(100), location VARCHAR(200))`;
+    await db.run(sql);
+
+    //sql = `CREATE TABLE IF NOT EXISTS `
+    await db.run(`INSERT INTO settings (currency, location) VALUES ('USD', 'United State of America')`)
         
 }
 
@@ -74,11 +75,15 @@ async function getItem(table,id) {
 }
 
 async function addIncomeExpense(item) {
+    console.log(Date.parse(item.transaction_date), 'number value');
+    console.log(item.transaction_date, 'main date')
+    item.transaction_date = Date.parse(item.transaction_date);
+    console.log(Number(item.transaction_date), "trans date");
     return new Promise((acc, rej) => {
         item.transaction_date = new Date()
         db.run(
-            `INSERT INTO income_expense (type, title, description, amount, is_recurrent, recurrency) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-            [item.type, item.name, item.completed ? 1 : 0],
+            `INSERT INTO income_expense (type, title, description, amount, is_recurrent, recurrency, transaction_date) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [item.type, item.title, item.description, item.amount, item.is_recurrent, item.recurrency, item.transaction_date ],
             (err, data) => {
                 if (err) return rej(err);
                 acc({
@@ -94,8 +99,8 @@ async function addAssetLiability(item) {
     return new Promise((acc, rej) => {
         item.transaction_date = new Date()
         db.run(
-            `INSERT INTO asset_liability (type, title, description, amount, change_rate, change_duration) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-            [item.type, item.name, item.completed ? 1 : 0],
+            `INSERT INTO asset_liability (type, title, description, amount, change_rate, change_duration) VALUES (?, ?, ?, ?, ?, ?)`,
+            [item.type, item.title, item.description],
             (err, data) => {
                 if (err) return rej(err);
                 acc({
@@ -142,29 +147,27 @@ async function removeItem(id) {
 }
 
 async function getFinancialStatement(dataObject){
+    console.log(dataObject, "obj")
+    const from = Date.parse(dataObject.from); console.log(from, 'from');
+    const to = Date.parse(dataObject.to);
     return new Promise((acc,rej)=>{
         const income_expenses = new Promise((resolve,reject)=>{
-            db.run(`SELECT * FROM income_expense WHERE transaction_date BETWEEN #${dataObject.from}# AND #${dataObject.to}#`, (err, data)=>{
+            db.all(`SELECT * FROM income_expense WHERE transaction_date > ${from}`, (err, data)=>{
                 if(err) reject(err)
-                else resolve(data)
+                else {console.log(JSON.stringify(data), 'data1');resolve(data)}
             })
         })
 
         const asset_liabilities = new Promise((resolve,reject)=>{
-            db.run(`SELECT * FROM asset_liability WHERE transaction_date BETWEEN #${dataObject.from}# AND #${dataObject.to}#`, (err, data)=>{
+            db.all(`SELECT * FROM asset_liability WHERE transaction_date BETWEEN '${dataObject.from}' AND '${dataObject.to}'`, (err, data)=>{
                 if(err) reject(err)
-                else resolve(data)
+                else {console.log(data, 'data2');resolve(data)}
             })
         })
         
         Promise.allSettled([income_expenses, asset_liabilities])
-        .then(results=>resolve({
-            isSuccessful: true,
-            data:{
-                income: results[0].value,
-                assets: results[0].value
-            }
-        }))
+        .then(results=>console.log(results[1].value,'after'))
+        .catch(error=>console.log(error, 'error'))
     })
 }
 
