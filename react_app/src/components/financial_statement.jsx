c6b34c7e24fae195ea6e004f78493c037df6c1ba
CodeLuminary <victorijoni@yahoo.com>
2022-07-28 23:21:59 +0100

    Generation of financial statement added

--- react_app/src/components/financial_statement.jsx
+++ react_app/src/components/financial_statement.jsx
@@ -3,7 +3,9 @@ import {getFinancialStatement} from "../helperFunctions/Api_communication.js";
 import {useState} from "react";
 
 const FinancialStatement = ()=>{
-    const [financialStatement, setFinancialStatement] = useState({})
+    const [incomeExpense, setIncomeExpense] = useState({});
+    const [assetLiabiltiy, setAssetLiability] = useState({});
+    const [incomeYear, setIncomeYear] = useState({});
     const [from, setFrom] = useState('');
     const [to, setTo] = useState('');
 
@@ -13,7 +15,51 @@ const FinancialStatement = ()=>{
             from, to
         })
         .then(result=>{
-            console.log(result, 'result')
+            console.log(result, 'result');
+            const income = {}; const expenses = {};
+            const incomeYear = {}
+            result.income_expenses.forEach((value)=>{
+                
+                const year = new Date(value.transaction_date).getFullYear();
+                incomeYear[year] = 1;
+                if(value.type=='income'){
+                    if(!income[value.title]) income[value.title] = {}
+                    income[value.title][year] = value.amount;
+                }
+                else{
+                    if(!expenses[value.title]) expenses[value.title] = {}
+                    expenses[value.title][year] = value.amount;
+                }
+
+                
+            })
+
+            income['Gross Profit'] = {}
+            expenses['Total Expenses'] = {}
+            expenses['Net Profit'] = {}
+                    
+            for(let x in incomeYear){
+                let incomeTotal = 0; let expensesTotal = 0;
+                for(let y in income){
+                    if(income[y][x])
+                        incomeTotal += income[y][x];
+                }
+                for(let y in expenses){
+                    if(expenses[y][x])
+                        expensesTotal += expenses[y][x];
+                }
+                income['Gross Profit'][x] = incomeTotal;
+                expenses['Total Expenses'][x] = expensesTotal;
+                expenses['Net Profit'][x] = incomeTotal - expensesTotal;
+            }
+            console.log(income, 'income')
+            setIncomeExpense({
+                ...income,
+                ...expenses
+            });
+            setIncomeYear(incomeYear);
+
+            
         })
         .catch(error=>console.log(error));
         //console.log(fs['from'])
@@ -27,15 +73,41 @@ const FinancialStatement = ()=>{
                 <span className={inc_exp_style.label}>To</span>
                 <input type="date" onChange={e=>setTo(e.target.value)} className="input" /><br/>
                 <input type="submit" className="btn" value="Get Financial Statement"/>
-            </form>
-            <>
-                {financialStatement.income ?
-                (
-                    <>
+            
+            
+                {Object.keys(incomeExpense).length > 0 ?
+                (   <>
+                    <h3>INCOME STATEMENT</h3>
+                    <table className={inc_exp_style.fs_tab}>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                {
+                                    Object.keys(incomeYear).map((year,index)=>(
+                                        <th key={index}>{year}</th>
+                                    ))
+                                }
+                            </tr>
+                        </thead>
+                        <tbody>    
+                            {
+                                Object.keys(incomeExpense).map((incExp, index)=>(
+                                    <tr className={incExp=='Gross Profit' || incExp=='Total Expenses'? inc_exp_style.draw: ``} key={index}>
+                                        <td>{incExp}</td>
+                                        {
+                                            Object.keys(incomeYear).map((year,index)=>(
+                                                <td key={index}>{incomeExpense[incExp][year]? incomeExpense[incExp][year]: `-`}</td>
+                                            ))
+                                        }
+                                    </tr>
+                                ))
+                            }    
+                        </tbody>
+                    </table>
                     </>
                 ):``          
                 }
-            </>
+            </form>
         </>
     )
 }
